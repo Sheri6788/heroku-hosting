d8db3dd319ed49cce890b7d2115956cc2824736b
sheri6788 <et.shahrzad67@gmail.com>
2023-09-15 10:16:00 -0400

    removed files

--- Includes/DB.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$DSN='mysql:host=localhost;dbname=cms4.2.1';
-$connectingDB = new PDO ('mysql:host=localhost;dbname=cms4.2.1','root','');
-
-?>
--- Includes/DateTime.php
+++
@@ -1,5 +0,0 @@
-<?php
-date_default_timezone_set ("America/New_York"); //change the timezone.
-$CurrentTime = time(); //get the time in seconds.
-$DateTime = strftime ("%m-%d-%y %H:%M:%S",$CurrentTime); //change time format, timezone is XAMPP timezone.
-?>
