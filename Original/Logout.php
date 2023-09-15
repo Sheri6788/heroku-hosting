c271b7dbc7ed5f26d1ff160f3e27d9bd4edf8770
sheri6788 <et.shahrzad67@gmail.com>
2023-09-14 21:33:02 -0400

    changes

--- Original/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php require_once ("includes/Functions.php");?>
-<?php require_once ("includes/Sessions.php");?>
-<?php 
-    $_SESSION["User_ID"] =null;
-    $_SESSION["UserName"] =null;
-    $_SESSION["AdminName"] =null;
-    session_destroy(); //TO DESTROY THE SESSION
-    Redirect_to ("Login.php");
-?>
